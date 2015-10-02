32461ae0542980b7b08e9b26aa38aebdc01920f8
Casper Steinmann <casper.steinmann@gmail.com>
2015-10-02 16:13:44 +0200

    Adds code and various stuff.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="Dalton-LoProp",
+    version="0.1",
+    scripts=["bin/dalton_loprop",],
+    author="Casper Steinmann",
+    author_email="casper.steinmann@gmail.com",
+    )
+
+    
